e92f77f64646cdaed265b12144c04a012a902fa5
DanerSound <andre.barreto28@gmail.com>
2018-03-27 17:46:07 +0200

    updated

--- ValueGenerator/src/base/ValueGenerator.java
+++ ValueGenerator/src/base/ValueGenerator.java
@@ -1,5 +1,11 @@
 package base;
 
 public class ValueGenerator {
-
+	
+public static int[] generatedNumbers(int[]vett){	
+		for (int i = 0; i < vett.length; i++) {
+			vett[i]=(int)(Math.random()*100);
+		}
+		return vett;
+	} 
 }
