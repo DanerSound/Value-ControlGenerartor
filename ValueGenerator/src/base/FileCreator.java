793714f1f57763e9228b1d70dae71a7f66f45c7e
DanerSound <andre.barreto28@gmail.com>
2018-03-27 17:27:04 +0200

    fixed indentation and formalitation

--- ValueGenerator/src/base/FileCreator.java
+++ ValueGenerator/src/base/FileCreator.java
@@ -8,71 +8,52 @@ import java.util.Scanner;
 import java.io.IOException;
 
 public class FileCreator {
-	
+
 	public static void CreatorFile() {
-		  try {
-	            File file = new File("sampleFile.txt");
-	            if(file.createNewFile())
-	                System.out.println("Success!");
-	                WriterOnFile();
-	        }
-	        catch(IOException io) {
-	            io.printStackTrace();
-	        }
-		}
-    
-	public static void PrinterFile(){
-		  File x = new File("sampleFile.txt");
-		  Scanner sc;
 		try {
-		    sc = new Scanner(x);
-		    while(sc.hasNext()) {
-	              System.out.println(sc.next());
-	          }
-	          sc.close();	
+			File file = new File("sampleFile.txt");
+			if (file.createNewFile()){
+				System.out.println("Success!");
+				WriterOnFile();
+				}
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
+	}
+
+	public static void PrinterFile() {
+		File x = new File("sampleFile.txt");
+		Scanner sc;
+		try {
+			sc = new Scanner(x);
+			while (sc.hasNext()) {
+			System.out.println(sc.next());
+			}
+			sc.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
-         
+	  }
 	}
-	
-	private static void WriterOnFile(){
-		
+
+	private static void WriterOnFile() {
 		BufferedWriter bw = null;
 		FileWriter fw = null;
 
 		try {
-
-			String content = "ciao Chiara!\n";
-
+			String content = "TEST STRING \n"; // LINE IN
 			fw = new FileWriter("sampleFile.txt");
 			bw = new BufferedWriter(fw);
 			bw.write(content);
-
 			System.out.println("Done");
-
 		} catch (IOException e) {
-
 			e.printStackTrace();
-
 		} finally {
-
 			try {
-
-				if (bw != null)
-					bw.close();
-
-				if (fw != null)
-					fw.close();
-
+				if (bw != null) bw.close();
+				if (fw != null) fw.close();
 			} catch (IOException ex) {
-
 				ex.printStackTrace();
-
 			}
-
 		}
-
 	}
-}	
-	
+}
