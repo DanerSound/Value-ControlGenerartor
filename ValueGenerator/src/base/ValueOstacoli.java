0c0f4e8d2f1bbd45d73c53beaf7dd6d3d25b5a77
DanerSound <andre.barreto28@gmail.com>
2018-03-30 13:01:53 +0200

    fixed index problem

--- ValueGenerator/src/base/ValueOstacoli.java
+++ ValueGenerator/src/base/ValueOstacoli.java
@@ -9,16 +9,14 @@ public class ValueOstacoli extends WriteOnFile {
 
 	public Object[] GenerarteOstacoliValues(Object[] vett) {
 		String alphabet = "AB";
-		int misures = 0;
-		int arrayIndex = 0;
-
-		while (misures != 100) {
+		int controlIndex=0;
+		
+		while (controlIndex!= 100) {
 			Integer distance = 0;
 			char ostacoloType = alphabet.charAt((new Random()).nextInt(alphabet.length()));
 			distance = (int) (Math.random() * 100);
-			vett[arrayIndex] = ostacoloType + distance.toString();
-			arrayIndex++;
-			misures++;
+			vett[controlIndex] = ostacoloType + distance.toString();
+			controlIndex++;
 		}
 		return vett;
 	}
