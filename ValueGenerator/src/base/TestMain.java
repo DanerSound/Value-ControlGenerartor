496967bb99579296c2a0ab27be10a81f646b72df
DanerSound <andre.barreto28@gmail.com>
2018-04-01 16:25:03 +0200

    dummy fix

--- ValueGenerator/src/base/TestMain.java
+++ ValueGenerator/src/base/TestMain.java
@@ -8,11 +8,11 @@ public class TestMain {
 		Object[]values = new Object[100];	
 		String[]fileNames={"SterzoValues.txt","PendenzaValues.txt","OstacoliValues.txt"};
 		
-//		values=ValueSterzo.generatedNumbers(values);
-//		WriteOnFile.writeValues(values,fileNames[0]);
-//
-//		values=ValuePendenza.randInt(-60, 60, values);
-//		WriteOnFile.writeValues(values,fileNames[1]);
+		values=ValueSterzo.generatedNumbers(values);
+		WriteOnFile.writeValues(values,fileNames[0]);
+
+		values=ValuePendenza.randInt(-60, 60, values);
+		WriteOnFile.writeValues(values,fileNames[1]);
 		
 		values=ValueOstacoli.GenerarteOstacoliValues(values);
 		WriteOnFile.writeValues(values,fileNames[2]);
