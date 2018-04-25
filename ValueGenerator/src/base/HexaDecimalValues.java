10831840cf46f3990eda74dffc17de55b31ac892
DanerSound <andre.barreto28@gmail.com>
2018-04-25 15:35:48 +0200

    added new class for generation of hexadecimal values and minor refactor

--- ValueGenerator/src/base/HexaDecimalValues.java
+++ ValueGenerator/src/base/HexaDecimalValues.java
@@ -8,13 +8,10 @@ public class HexaDecimalValues {
 	
 	public static String[] generate ( int numberOfElement, String distanceAlphabet){
 		String [] vett = new String[numberOfElement];
-		char[] distance= new char[2];
-		
+
 		for (int index=0;index<numberOfElement;index++){
 			char obsType= obstacleType.charAt((new Random()).nextInt(obstacleType.length()));
-			distance[0]=distanceAlphabet.charAt(new Random().nextInt(distanceAlphabet.length()));
-			distance[1]=distanceAlphabet.charAt(new Random().nextInt(distanceAlphabet.length()));
-			vett[index]=obsType+String.valueOf(distance[0])+String.valueOf(distance[1]);
+			vett[index]=obsType+NumberHexadecimal.generate(distanceAlphabet)+NumberHexadecimal.generate(distanceAlphabet);
 		}
 	 return vett;
 	}	
--- ValueGenerator/src/base/IntegerValuesInRange.java
+++ ValueGenerator/src/base/IntegerValuesInRange.java
@@ -3,7 +3,7 @@ package base;
 import java.util.Random;
 
 public class IntegerValuesInRange {
-	public static String[] generate (int min, int max, int numberOfElement) {
+	public static String[] generate (int min, int max, int numberOfElement){
 		String vett[] = new String[numberOfElement];
 		for (int i = 0; i < numberOfElement ; i++) {
 			vett[i] = String.valueOf((new Random()).nextInt((max - min) + 1) + min);
