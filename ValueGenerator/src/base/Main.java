4331f56a61b1f60c173b75961e986c0e34a8c7bc
DanerSound <andre.barreto28@gmail.com>
2018-05-24 11:26:01 +0200

    edited main

--- ValueGenerator/src/base/Main.java
+++ ValueGenerator/src/base/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		WriteOnFile.writeValues(IntegerValuesInRange.generate(-60, 60, 100), "pendenzaIN.txt");
+		WriteOnFile.writeValues(IntegerValuesInRange.generate(-180,180, 100), "pendenzaIN.txt");
 		
-		WriteOnFile.writeValues(IntegerValuesInRange.generate(0, 100, 100), " sterzoIN.txt");
+		WriteOnFile.writeValues(IntegerValuesInRange.generate(-180, 180, 100), " sterzoIN.txt");
 		
-		WriteOnFile.writeValues(HexaDecimalValues.generate(100, "123456789ABCDEF"), "distanzaIN.txt");	
+		WriteOnFile.writeValues(HexaDecimalValues.generate(100, "0123456789ABCDEF"), "distanzaIN.txt");	
 	}
 }
